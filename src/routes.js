949142c49985473a9a701a6c43343b35ff0fe8e0
unknown <rjtool@rams.colostate.edu>
2021-11-28 14:49:59 -0700

    13.1 11/28 add page components, routing

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "./components/Home.vue";
+import Login from "./components/Login.vue";
+import EventDetail from "./components/EventDetail.vue";
+import Events from "./components/Events.vue";
+import NotFound from "./components/NotFound.vue";
+import EventEntry from "./components/EventEntry.vue";
+import Signup from "./components/Signup.vue";
+import Account from "./components/Account.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/account", component: Account },
+    { path: "/login", component: Login },
+    { path: "/events", component: Events },
+    {
+      path: "/events/:pk",
+      component: EventDetail,
+      children: [{ path: "/entry", component: EventEntry }],
+    },
+    { path: "/signup", component: Signup },
+    { path: "/:invalidroute(.*)", component: NotFound },
+  ],
+});
+
+export default router;
