7cc363c9fa9f11af8e35c0861ed565e4f6011793
unknown <rjtool@rams.colostate.edu>
2021-12-12 10:16:06 -0700

    12/12 updated home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Home</h1>
+    <h2>Welcome to Tool Events! Your source for all bookings at Tool Arena!</h2>
+    <h3>Sign in to log your entry for a chance to win prizes!</h3>
   </div>
 </template>
 
